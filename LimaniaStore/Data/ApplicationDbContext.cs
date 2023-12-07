2c040fcd793cc42214f312da4e6cc68c9fbd8552
Sezer <limanlarsezer@gmail.com>
2023-12-08 01:16:06 +0300

    db

---
+++ LimaniaStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LimaniaStore.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+
+
+	}
+}
--- LimaniaStore/Program.cs
+++ LimaniaStore/Program.cs
@@ -1,3 +1,6 @@
+using LimaniaStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace LimaniaStore
 {
 	public class Program
@@ -8,6 +11,10 @@ namespace LimaniaStore
 
 			// Add services to the container.
 			builder.Services.AddControllersWithViews();
+			builder.Services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 			var app = builder.Build();
 
