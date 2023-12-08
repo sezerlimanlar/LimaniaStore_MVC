866ddcb6557c5b870f2c3f8813ad57ea1d9dcb27
Sezer <limanlarsezer@gmail.com>
2023-12-08 20:26:00 +0300

    category

---
+++ LimaniaStore/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using LimaniaStore.Data;
+using LimaniaStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LimaniaStore.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public CategoryController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			List<Category> datas = await _db.Categories.ToListAsync();
+			return View(datas);
+
+		}
+
+	}
+}
---
+++ LimaniaStore/Migrations/20231208163122_SeedCategoryTable.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LimaniaStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Aksiyon" },
+                    { 2, 2, "Bilim Kurgu" },
+                    { 3, 3, "Macera" },
+                    { 4, 4, "Komedi" },
+                    { 5, 5, "Romantik" },
+                    { 6, 6, "Korku" },
+                    { 7, 7, "Gerilim" },
+                    { 8, 8, "Çocuk" },
+                    { 9, 9, "Yetişkin" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 9);
+        }
+    }
+}
