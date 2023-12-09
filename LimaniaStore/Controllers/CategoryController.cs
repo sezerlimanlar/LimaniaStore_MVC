67e901ea6ff7deec1cdb2a702ff8e182aa4127d2
Sezer <limanlarsezer@gmail.com>
2023-12-10 02:47:02 +0300

    some additions

--- LimaniaStore/Controllers/CategoryController.cs
+++ LimaniaStore/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using LimaniaStore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace LimaniaStore.Controllers
 {
@@ -19,6 +20,62 @@ namespace LimaniaStore.Controllers
 			return View(datas);
 
 		}
+		public async Task<IActionResult> Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Create(Category category)
+		{
+			/*  if(category.Name == category.DisplayOrder.ToString())
+			  {
+				  // İlk parametre hangi input altında error mesajının gözükmesini istiyorsak onu belirtir. (Custom error message)
+				  ModelState.AddModelError("", "Kategori ismi ve kategori sıralaması aynı isim girilemez."); 
+			  }*/
+			if (ModelState.IsValid)
+			{
 
+				await _db.Categories.AddAsync(category);
+				TempData["success"] = "Kategori Ekleme Başarılı!";
+				await _db.SaveChangesAsync();
+				return RedirectToAction("Index", "Category");
+			}
+			return View();
+		}
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? category = await _db.Categories.FindAsync(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Edit(Category category)
+		{
+			_db.Categories.Update(category);
+			TempData["success"] = "Kategori Düzenleme Başarılı!";
+			_db.SaveChanges();
+			return RedirectToAction("Index", "Category");
+		}
+
+		public async Task<IActionResult> Delete(int? id)
+		{
+			Category? obj = await _db.Categories.FindAsync(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			_db.Categories.Remove(obj);
+			TempData["success"] = "Kategori Silme Başarılı!";
+			await _db.SaveChangesAsync();
+			return RedirectToAction("Index", "Category");
+		}
 	}
 }
--- LimaniaStore/Models/Category.cs
+++ LimaniaStore/Models/Category.cs
@@ -1,13 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace LimaniaStore.Models
 {
-	public class Category
-	{
-		[Key]
-		public int Id { get; set; }
-		[Required]
-		public string Name { get; set; }
-		public int DisplayOrder { get; set; }
-	}
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Kategori ismi boş geçilemez")]
+        [DisplayName("Kategori İsmi")]
+        [MaxLength(30, ErrorMessage = "Maks limit 30 karakter.")]
+        public string Name { get; set; }
+        [DisplayName("Kategori Sıralaması")]
+        [Range(1, 100, ErrorMessage = "Girilen değer aralığı 1-100 arası olmalı")]
+        public int DisplayOrder { get; set; }
+    }
 }
