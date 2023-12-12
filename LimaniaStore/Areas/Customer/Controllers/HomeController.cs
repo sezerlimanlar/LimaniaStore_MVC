81444ec76c298b5fc7dca1ca46935d44b2025f79
Sezer <limanlarsezer@gmail.com>
2023-12-13 02:20:14 +0300

    details page

--- LimaniaStore/Areas/Customer/Controllers/HomeController.cs
+++ LimaniaStore/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Limania.DataAccess.Repository.IRepository;
 using Limania.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,17 +8,27 @@ namespace LimaniaStore.Areas.Customer.Controllers
 	[Area("Customer")]
 	public class HomeController : Controller
 	{
+		private readonly IUnitOfWork _unitOfWork;
 		private readonly ILogger<HomeController> _logger;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
 		{
 			_logger = logger;
+			_unitOfWork = unitOfWork;
 		}
 
 		public IActionResult Index()
 		{
-			return View();
+			IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+			return View(productList);
 		}
+
+		public async Task<IActionResult> Details(int productId)
+		{
+			Product product = await _unitOfWork.Product.Get(p => p.Id == productId, includeProperties: "Category");
+			return View(product);
+		}
+
 
 		public IActionResult Privacy()
 		{
